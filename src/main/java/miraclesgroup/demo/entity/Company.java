a527844cc9a4d9b76ad07a516f86d53165f8a9c9
Cuong Nguyen <cuongnguyenhatinh@gmail.com>
2020-03-14 11:21:30 +0700

    create/entity

---
+++ src/main/java/miraclesgroup/demo/entity/Company.java
@@ -0,0 +1,26 @@
+package miraclesgroup.demo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Table(name = "company")
+@Data
+@Entity
+public class Company implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", insertable = false, nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    
+}
